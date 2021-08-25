13e2f0e9d5045a4ff73871659d0c15cd244fd7d9
Behzod <uzpythonistdeveloper@gmail.com>
2021-08-25 20:58:56 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from aiogram import Bot,Dispatcher,executor,types
+import requests
+import json
+token='1983534574:AAEX7aEsJZSSwWxEAgitKmtwEouEgGQy8sw'
+bot=Bot(token=token)
+dp=Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start(message:types.Message):
+    pic=open('pic.jpeg','rb')
+    caption='<b>Assalomu aleykum!Botga Xush kelibsiz!</b>'+'\n'+'<b>Bot sizga uzun url manzilni qisqartirib beradi!</b>'
+    await bot.send_photo(message.chat.id,pic,caption=caption,parse_mode='html')
+@dp.message_handler(content_types=['text'])
+async def asosiy(message:types.Message):
+    text=message.text
+    key='e52a3986215cb1ffc4e71e1242a3939365ffd'
+    import requests
+    link=text
+    url='http://cutt.ly/api/api.php?key={}&short={}'.format(key,text)
+    r = requests.get(url)
+    a=json.loads(r.text)
+    shortlink=a['url']['shortLink']
+    short='https://github.com/public-apis/public-apis#development'
+    qrcode='https://api.qrserver.com/v1/create-qr-code/?size=150x150&data={}'.format(text)
+    caption='Qisqartirilgan url manzil: '+shortlink+'\n'+'Asl url manzil: '+text+'\n'+'Bizning bot: @shortenurl_robot'
+    await bot.send_photo(message.chat.id,qrcode,caption=caption)
+
+if __name__=='__main__':
+    executor.start_polling(dp)
